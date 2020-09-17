78a966a6346a3d0c4e4c47a29e937e28a67349c2
WannabeRockStar <30549855+WannabeRockStar@users.noreply.github.com>
2020-09-18 00:22:41 +0400

    Updates

---
+++ precache-manifest.18f5bd5839b36fb6e9c7ebac74a5cccb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49db95d03a2a7568ef606fe67ab46437",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "5dd6df3140f4f75befb0",
+    "url": "/task/static/css/main.017307ab.chunk.css"
+  },
+  {
+    "revision": "fc028d1bc1e5f11aaeba",
+    "url": "/task/static/js/2.634218ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task/static/js/2.634218ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd6df3140f4f75befb0",
+    "url": "/task/static/js/main.3fbbd45e.chunk.js"
+  },
+  {
+    "revision": "e5284e5de2396dc1be1f",
+    "url": "/task/static/js/runtime-main.cf74d6d5.js"
+  },
+  {
+    "revision": "92bc505440ecacb5f7c13f17fdfd36d8",
+    "url": "/task/static/media/Clip.92bc5054.png"
+  },
+  {
+    "revision": "a6509aa3161205ebe01c0f7e2f209b44",
+    "url": "/task/static/media/LeagueSpartan-Bold.a6509aa3.otf"
+  },
+  {
+    "revision": "d27b6b12d96d9cf68f493c78113ce390",
+    "url": "/task/static/media/PlayfairDisplay-Bold.d27b6b12.ttf"
+  },
+  {
+    "revision": "b3721ba3bde34e5b38b0e1523cccfd7f",
+    "url": "/task/static/media/PlayfairDisplay-Regular.b3721ba3.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/task/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "5e6e23c2ed04de98352b78f572a82958",
+    "url": "/task/static/media/baze.5e6e23c2.png"
+  },
+  {
+    "revision": "552f9e004c23fc700bcbad09d033c589",
+    "url": "/task/static/media/beach.552f9e00.png"
+  },
+  {
+    "revision": "3dc46ed57da577b6ed0235a2b66e513f",
+    "url": "/task/static/media/blog.3dc46ed5.png"
+  },
+  {
+    "revision": "986c307fc5444bafca4c290bf9617ead",
+    "url": "/task/static/media/board1.986c307f.png"
+  },
+  {
+    "revision": "812d24895d67adb6723cb2a7dd01366f",
+    "url": "/task/static/media/board2.812d2489.png"
+  },
+  {
+    "revision": "06799fc3a4f694bf75974226ad1749de",
+    "url": "/task/static/media/board3.06799fc3.png"
+  },
+  {
+    "revision": "c4f659b8a2cf5744a177b8ddfc83312b",
+    "url": "/task/static/media/img.c4f659b8.png"
+  },
+  {
+    "revision": "88fdfcbef0047e21683d17036dc69937",
+    "url": "/task/static/media/wave.88fdfcbe.png"
+  }
+]);
